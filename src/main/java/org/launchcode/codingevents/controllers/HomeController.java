5e6cfbddc8fd73d62f20b1520248b8041a136da2
Jimmy <hungtran2003ti@gmail.com>
2024-01-24 21:40:38 -0600

    Model + Model binding

--- src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -1,13 +1,14 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @GetMapping
-    public String index() {
+    public String index(Model model) {
         return "index";
     }
 }
